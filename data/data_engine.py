95bc3363677e239ecd5b6107f3fe4a680fe180a8
zakariaabdallah756-star <Zakariaabdallah756@gmail.com>
2026-07-08 19:36:36 +0200

    Validate candles before saving

--- data/data_engine.py
+++ data/data_engine.py
@@ -1,13 +1,18 @@
 from data.market_stream import MarketStream
 from data.candle_repository import CandleRepository
-
+from data.validator import CandleValidator
 
 class DataEngine:
     def __init__(self):
         self.stream = MarketStream()
         self.repository = CandleRepository()
+        self.validator = CandleValidator()
 
     def update(self):
         candle = self.stream.get_next_candle()
-        self.repository.add(candle)
-        return candle
+
+        if self.validator.validate(candle):
+            self.repository.add(candle)
+            return candle
+
+        return None
