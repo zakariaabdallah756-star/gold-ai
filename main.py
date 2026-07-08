afad8e5cfa1eae83ec8d7f7db5ee65a60651f92c
zakariaabdallah756-star <Zakariaabdallah756@gmail.com>
2026-07-08 10:28:05 +0200

    Add event definitions

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from core.logger import logger
 from core.constants import APP_NAME, VERSION
 from core.database import engine
 from core.utils import generate_id
+from core.events import Event
+
 def main():
     logger.info(f"{APP_NAME} v{VERSION} avviato.")
     print(f"{APP_NAME} v{VERSION} avviato correttamente.")
     print(engine)
     print(generate_id())
+    print(Event.NEW_CANDLE)
 if __name__ == "__main__":
     main()
