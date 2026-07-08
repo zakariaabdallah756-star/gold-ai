4761f3fadb9f6ef70ddccd51ec31d5b5a8e40c67
zakariaabdallah756-star <Zakariaabdallah756@gmail.com>
2026-07-08 10:06:21 +0200

    Use .env configuration

--- core/config.py
+++ core/config.py
@@ -1,6 +1,16 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+
+APP_NAME = os.getenv("APP_NAME")
+VERSION = os.getenv("VERSION")
+SYMBOL = os.getenv("SYMBOL")
+TIMEFRAME = os.getenv("TIMEFRAME")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 LOG_DIR = BASE_DIR / "logs"
 DATA_DIR = BASE_DIR / "data"
