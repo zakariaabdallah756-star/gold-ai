3ecc94b0c8f0e164f511ecca348f071f522dafd4
zakariaabdallah756-star <Zakariaabdallah756@gmail.com>
2026-07-10 09:12:20 +0200

    Integrate PositionCloser into PositionUpdater

--- paper/position_updater.py
+++ paper/position_updater.py
@@ -2,22 +2,27 @@ from paper.position_manager import PositionManager
 from paper.profit_loss import ProfitLossCalculator
 from paper.account_manager import AccountManager
 from paper.paper_account import PaperAccount
-
+from paper.position_closer import PositionCloser
 
 class PositionUpdater:
 
     def __init__(self):
         self.pnl = ProfitLossCalculator()
         self.account_manager = AccountManager()
-
+        self.closer = PositionCloser()
     def update(
-        self,
-        manager: PositionManager,
-        account: PaperAccount,
-        current_price: float,
-    ):
+    self,
+    manager: PositionManager,
+    account: PaperAccount,
+    current_price: float,
+        ):
         for position in manager.get_open_positions():
+
             profit_loss = self.pnl.calculate(position, current_price)
+
             self.account_manager.update_balance(account, profit_loss)
+
+            if self.closer.should_close(position, current_price):
+                manager.close_position(position)
 
         return account
