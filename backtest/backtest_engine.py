eefddc2048a2e136826c7f795a7d12c9b4435f76
zakariaabdallah756-star <Zakariaabdallah756@gmail.com>
2026-07-11 02:45:02 +0200

    Connect IndicatorEngine to Backtest

--- backtest/backtest_engine.py
+++ backtest/backtest_engine.py
@@ -1,14 +1,25 @@
 from market.data_engine import DataEngine
-
-
+from strategy.strategy_engine import StrategyEngine
+from indicators.indicator_engine import IndicatorEngine
+from strategy.strategy_engine import StrategyEngine
 class BacktestEngine:
 
     def __init__(self, data_engine: DataEngine):
         self.data_engine = data_engine
-
+        self.indicator_engine = IndicatorEngine()
+        self.strategy_engine = StrategyEngine()
     def load_data(self):
         return self.data_engine.get_candles()
     def run(self):
         candles = self.load_data()
-        for candle in candles:
-            print(candle)
+
+        if not candles:
+            print("Nessuna candela disponibile.")
+            return
+
+        indicators = self.indicator_engine.calculate(candles)
+
+        signal = self.strategy_engine.generate_signal(indicators)
+
+        print("Indicators:", indicators)
+        print("Signal:", signal)
