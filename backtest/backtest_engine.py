a11291661f35ec00c8fc19d12cc7b6f5378a38e4
zakariaabdallah756-star <Zakariaabdallah756@gmail.com>
2026-07-11 08:51:43 +0200

    Complete Backtest signal flow

--- backtest/backtest_engine.py
+++ backtest/backtest_engine.py
@@ -2,12 +2,14 @@ from market.data_engine import DataEngine
 from strategy.strategy_engine import StrategyEngine
 from indicators.indicator_engine import IndicatorEngine
 from strategy.strategy_engine import StrategyEngine
+from risk.risk_engine import RiskEngine
 class BacktestEngine:
 
     def __init__(self, data_engine: DataEngine):
         self.data_engine = data_engine
         self.indicator_engine = IndicatorEngine()
         self.strategy_engine = StrategyEngine()
+        self.risk_engine = RiskEngine()
     def load_data(self):
         return self.data_engine.get_candles()
     def run(self):
