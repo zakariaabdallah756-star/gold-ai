bd59367492d18950d75732adfc25bb7fb36dfd3c
zakariaabdallah756-star <Zakariaabdallah756@gmail.com>
2026-07-14 14:57:29 +0200

    Connect StrategyEngine to GoldStrategy

--- strategy/strategy_engine.py
+++ strategy/strategy_engine.py
@@ -1,9 +1,10 @@
-from strategy.moving_average_strategy import MovingAverageStrategy
+from strategy.gold_strategy import GoldStrategy
 
 
 class StrategyEngine:
+
     def __init__(self):
-        self.strategy = MovingAverageStrategy()
+        self.strategy = GoldStrategy()
 
     def generate_signal(self, indicators):
         return self.strategy.generate(indicators)
