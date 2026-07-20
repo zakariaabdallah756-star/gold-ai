419de8a5326d266071c7bcc86cc86602b63f95e8
zakariaabdallah756-star <Zakariaabdallah756@gmail.com>
2026-07-20 11:46:38 +0200

    Connect BreakoutStrategy to StrategyEngine

--- strategy/strategy_engine.py
+++ strategy/strategy_engine.py
@@ -1,6 +1,7 @@
 from strategy.gold_strategy import GoldStrategy
 from strategy.market_regime import MarketRegimeDetector, MarketRegime
 from strategy.trend_following_strategy import TrendFollowingStrategy
+from strategy.breakout_strategy import BreakoutStrategy
 
 
 class StrategyEngine:
@@ -9,6 +10,7 @@ class StrategyEngine:
         self.strategy = GoldStrategy()
         self.market_regime_detector = MarketRegimeDetector()
         self.trend_strategy = TrendFollowingStrategy()
+        self.breakout_strategy = BreakoutStrategy()
 
     def generate_signal(self, indicators):
         market_regime = self.market_regime_detector.detect(indicators)
@@ -17,5 +19,8 @@ class StrategyEngine:
 
         if market_regime == MarketRegime.TREND:
             return self.trend_strategy.generate(indicators)
+
+        if market_regime == MarketRegime.BREAKOUT:
+            return self.breakout_strategy.generate(indicators)
 
         return self.strategy.generate(indicators)
