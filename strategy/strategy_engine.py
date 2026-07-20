49e9398cdb5d9dbe2a2de120407c00e6d7658108
zakariaabdallah756-star <Zakariaabdallah756@gmail.com>
2026-07-20 11:31:45 +0200

    Connect TrendFollowingStrategy to StrategyEngine

--- strategy/strategy_engine.py
+++ strategy/strategy_engine.py
@@ -1,5 +1,6 @@
 from strategy.gold_strategy import GoldStrategy
 from strategy.market_regime import MarketRegimeDetector, MarketRegime
+from strategy.trend_following_strategy import TrendFollowingStrategy
 
 
 class StrategyEngine:
@@ -7,10 +8,14 @@ class StrategyEngine:
     def __init__(self):
         self.strategy = GoldStrategy()
         self.market_regime_detector = MarketRegimeDetector()
+        self.trend_strategy = TrendFollowingStrategy()
 
     def generate_signal(self, indicators):
         market_regime = self.market_regime_detector.detect(indicators)
 
         print("Market Regime:", market_regime.value)
+
+        if market_regime == MarketRegime.TREND:
+            return self.trend_strategy.generate(indicators)
 
         return self.strategy.generate(indicators)
